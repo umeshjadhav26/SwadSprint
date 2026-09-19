055bbf372d3ebcb273daab423141ab41e7fa2d5e
Umesh Jadhav <umeshjadhav7402@gmail.com>
2026-09-20 00:03:28 +0530

    Prepare project for deployment

--- src/main/java/com/tap/utility/DBConnection.java
+++ src/main/java/com/tap/utility/DBConnection.java
@@ -1,32 +1,40 @@
 package com.tap.utility;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Connection;
 
 public class DBConnection {
-	
-	private static final String URL = "jdbc:mysql://localhost:3306/tap_food";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "Umesh";
-	
-	static Connection connection;
-	
-	public static Connection getConnection(){
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-			
-			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-		return connection;
-	}
 
+    private static final String URL =
+            System.getenv("DB_URL");
+
+    private static final String USERNAME =
+            System.getenv("DB_USERNAME");
+
+    private static final String PASSWORD =
+            System.getenv("DB_PASSWORD");
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(
+                    URL,
+                    USERNAME,
+                    PASSWORD
+            );
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
 }
